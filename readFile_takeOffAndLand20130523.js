3975ba032d5006ad12af4684afa31e93e12b18f1
Robert Talamantez <info@forestfieldinteractive.com>
2013-05-23 19:08:40 -0700

    Delay update
    This version of readFile_takeOffAndLand.js reads the incoming file at
    once a second, instead of 100000 times a second.  I haven't tested it
    with the quadcopter yet.

---
+++ readFile_takeOffAndLand20130523.js
@@ -0,0 +1,41 @@
+fs = require('fs');
+
+var arDrone = require('ar-drone');
+var client  = arDrone.createClient('192.168.1.1');
+var fs = require('fs');
+var benchmark=50;
+
+//read the file with a delay of 1 second
+getReading();
+
+function getReading(){
+	var array = fs.readFileSync('C:/users/rtalaman/documents/processing/Brain_Drone_Control/data/data.txt').toString().split("\n");
+	var attention=array[1];
+	console.log(attention);
+	if(attention>=benchmark){
+		fly();
+		}
+	setTimeout(getReading,5000);
+	
+	
+}
+//If your attention is over the benchmark, Run takeoff and land 
+function fly(){
+	{
+		client.takeoff();
+		client.after(1000, function() 
+			{
+			this.clockwise(0.5);
+			}
+					);
+					
+		client.after(5000, function() 
+			{
+		this.stop();
+		this.land();
+			}					
+					);
+  setTimeout(fly,10000);
+  i+=1;
+  }
+  }
