bf3e1682af3cd787171843e6020aab07980c59c2
Sam Heller <sheller@stelladot.com>
2014-06-05 01:18:08 -0700

    other stuff

--- neurosky/server.js
+++ neurosky/server.js
@@ -1,38 +1,150 @@
 var nodeThinkGear = require('node-neurosky');
 var http = require('http');
+var arDrone = require('ar-drone');
+// var arDrone = this.arDrone = require('ar-drone');
 
-var tgClient = nodeThinkGear.createClient({
-    appName: 'SomeSortaClient',
-    appKey: 'iamtheverymodelofamodernmajorgeneral'
-});
+var myObject = {
+	attention : 0,
+	currentState : "land",
+	drone : {},
+	setAttention : function(att){
+/*		console.log("Attention is ", att);
+		if ( att > 50 && myObject.currentState == "land"){
+			myObject.drone.takeoff();
+			myObject.currentState = "hover";
+			console.log("Start");
+		}
+		else if (att < 50 && myObject.currentState == "hover"){
+			myObject.drone.stop();
+			myObject.drone.land();
+			myObject.currentState = "land";
+			console.log("Stop");
+		}
+	},*/
 
-tgClient.on('data', function(data){
-	tgClient.currentData = data;
-	console.log(data);
-});
+		if (att < 50 && myObject.currentState == "hover"){
+			myObject.drone.stop();
+			myObject.drone.land();
+			myObject.currentState = "land";
+			console.log("Stop Edit");
+		} else if ( att > 50 && myObject.currentState == "land"){
+			myObject.drone.takeoff();
+			myObject.currentState = "hover";
+			console.log("Start Edit");
+		}
+	},
+	init : function(){
+		console.log("Initializing Droooooooone");
+		myObject.drone = arDrone.createClient('192.168.1.1');
+	},
+	badSignal : function(){
+		if (myObject.currentState == "hover"){
+			myObject.drone.stop();
+			myObject.drone.land();
+		}
+	}
 
-tgClient.connect();
+};
+droneConnect();
+
+runHeadset();
+function runHeadset(){
+	console.log("Running headset");
+	var tgClient = nodeThinkGear.createClient({
+	    appName: 'SomeSortaClient',
+	    appKey: 'iamtheverymodelofamodernmajorgeneral'
+	});
+  myObject.init();
+	tgClient.on('data', function(data){
+		//console.log(data);
+
+		if( typeof data.eSense.attention === "number"){
+			console.log("Setting attention?");
+			myObject.setAttention(data.eSense.attention);
+		} else {
+			//console.log(data);
+		}
+		if (typeof data.poorSignalLevel === "number" && data.poorSignalLevel !== 0){
+			console.log("Poor signal level : ", data.poorSignalLevel);
+			if (data.poorSignalLevel == 200){
+				myObject.badSignal();
+			}
+		} else if (typeof data.blinkStrength === "number"){
+			console.log("Blinked at ", data.blinkStrength);
+		} else {
+			myObject.setAttention(data.eSense.attention);
+		}
+	});
+
+	tgClient.connect();
+}
+
+function droneConnect(){
+	console.log("entering drone connect");
+	var arDrone = require('ar-drone');
+	console.log("drone loaded, connecting to client");
+	var client = arDrone.createClient('192.168.1.1');
+	console.log("Client connected");
+
+	client.takeoff();
+	console.log("Takeoff");
+
+	client.after(5000, function() {
+		console.log("Landing");
+	    this.stop();
+	    this.land();
+	  });
+
+}
 
 
-// tgClient.on('data', function(data){
-// 	if (typeof data.poorSignalLevel === "number" && data.poorSignalLevel !== 0){
-// 		console.log("Poor signal level : ", data.poorSignalLevel)
-// 	} else if (typeof data.blinkStrength === "number"){
-// 		console.log("Blinked at ", data.blinkStrength);
-// 	} else {
-// 		if (data.eSense.attention > data.eSense.meditation){
-// 			console.log("Attentive eh?");
-// 		} else {
-// 			console.log("Much zen, very calm");
-// 		}
-// 	}	
+// function connectHeadset(){
+// var tgClient = nodeThinkGear.createClient({
+//     appName: 'SomeSortaClient',
+//     appKey: 'iamtheverymodelofamodernmajorgeneral'
 // });
 
-var server = http.createServer(function(req, res){
-	res.writeHead(200);
-	res.write(JSON.stringify(tgClient.currentData));
-	res.end();
-	
-});
+// 	tgClient.currentData = {
+// 		'attention' : 0,
+// 		'meditation' : 0,
+// 		'delta' : 0,
+// 		'theta' : 0,
+// 		'blink' : 0,
+// 		'lastBlink' : 0
+// 	}
+// 	tgClient.on('data', function(data){
+// 		if (typeof tgClient.currentData.eSense.attention === "number"){
+// 		tgClient.currentData.attention = data.eSense.attention;
+// 		tgClient.currentData.meditation = data.eSense.attention;
+// 		tgClient.currentData.delta = data.eegPower.delta;
+// 		tgClient.currentData.theta = data.eegPower.theta.
+// 		}
+// 		console.log(data);
+// 	});
 
-server.listen(8181);
+// 	tgClient.connect();
+
+
+// 	// tgClient.on('data', function(data){
+// 	// 	if (typeof data.poorSignalLevel === "number" && data.poorSignalLevel !== 0){
+// 	// 		console.log("Poor signal level : ", data.poorSignalLevel)
+// 	// 	} else if (typeof data.blinkStrength === "number"){
+// 	// 		console.log("Blinked at ", data.blinkStrength);
+// 	// 	} else {
+// 	// 		if (data.eSense.attention > data.eSense.meditation){
+// 	// 			console.log("Attentive eh?");
+// 	// 		} else {
+// 	// 			console.log("Much zen, very calm");
+// 	// 		}
+// 	// 	}
+// 	// });
+
+// 	var server = http.createServer(function(req, res){
+// 		res.writeHead(200);
+// 		res.write(JSON.stringify(tgClient.currentData));
+// 		res.end();
+
+// 	});
+
+// 	server.listen(8181);
+// }
